2cbb51d5158c49ff1ad76f92ceb4216ae4cb7c7b
quesque <s.quesque@orange.fr>
2024-01-09 11:16:33 +0100

    depart

---
+++ app.js
@@ -0,0 +1,36 @@
+const responsesUser = ["c", "a", "b", "a", "c"];
+
+
+const lesMessages =[
+                    {
+                      message1 : "Euh...Vous le faites exprès ?",
+                      message2 :  "Retentez une autre réponse dans les cases rouges, puis re-validez !",
+                      emoji : "👎"
+                    },
+                    {
+                      message1 : "Peut mieux faire !",
+                      message2 :  "Retentez une autre réponse dans les cases rouges, puis re-validez !",
+                      emoji : "👎"
+                    },
+                    {
+                      message1 : "Il reste quelques erreurs.",
+                      message2 :  "Retentez une autre réponse dans les cases rouges, puis re-validez !",
+                      emoji : "😭"
+                    },
+                    {
+                      message1 : "Encore un effort ...",
+                      message2 :  "Retentez une autre réponse dans les cases rouges, puis re-validez !",
+                      emoji : "👀"
+                    },
+
+                    {
+                      message1 : "Vous y êtes presque !",
+                      message2 :  "Retentez une autre réponse dans la case rouge, puis re-validez !",
+                      emoji : "✨"
+                    },
+                    {message1 : "Bravo, c'est un sans faute !",
+                    message2 : "Quelle culture ...",
+                    emoji : "✔️"
+                    }
+                  ];
+
